1abb58d3b4fe8f298379fc30fcd2adcf31cb002e
Harry Lewis <harry@upmind.com>
2022-06-06 15:20:23 +0100

    Remove confusing protocol + port from WHMv1 configuration data set

--- src/WHMv1/Data/WHMv1Credentials.php
+++ src/WHMv1/Data/WHMv1Credentials.php
@@ -10,9 +10,7 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
 /**
  * WHMv1 API credentials.
  *
- * @property-read string|null $protocol API protocol (http or https)
  * @property-read string $hostname WHM server hostname
- * @property-read integer|null $port API port
  * @property-read string $whm_username WHM API username
  * @property-read string $api_key WHM API secret
  */
@@ -21,9 +19,7 @@ class WHMv1Credentials extends DataSet
     public static function rules(): Rules
     {
         return new Rules([
-            'protocol' => ['alpha', 'in:http,https'],
             'hostname' => ['required', 'domain_name'],
-            'port' => ['integer'],
             'whm_username' => ['required', 'alpha_num'],
             'api_key' => ['required', 'string'],
         ]);
