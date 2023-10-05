ba2ce166305a75a6cce03ba765d17ca4fe0ac91f
Harry Lewis <harry@upmind.com>
2023-10-05 17:18:35 +0100

    Fix formatting

--- src/SolidCP/Api.php
+++ src/SolidCP/Api.php
@@ -41,16 +41,16 @@ class Api
     public static function getFriendlyError($code)
     {
         $errors = [
-            -100  => 'Username not available, already in use',
-            -101  => 'Username not found, invalid username',
-            -102  => 'User\'s account has child accounts',
-            -300  => 'Hosting package could not be found',
-            -301  => 'Hosting package has child hosting spaces',
-            -501  => 'The sub-domain belongs to an existing hosting space that does not allow sub-domains to be created',
-            -502  => 'The domain or sub-domain exists in another hosting space / user account',
-            -511  => 'Preview Domain is enabled, but not configured',
-            -601  => 'The website already exists on the target hosting space or server',
-            -700  => 'The email domain already exists on the target hosting space or server',
+            -100 => 'Username not available, already in use',
+            -101 => 'Username not found, invalid username',
+            -102 => 'User\'s account has child accounts',
+            -300 => 'Hosting package could not be found',
+            -301 => 'Hosting package has child hosting spaces',
+            -501 => 'The sub-domain belongs to an existing hosting space that does not allow sub-domains to be created',
+            -502 => 'The domain or sub-domain exists in another hosting space / user account',
+            -511 => 'Preview Domain is enabled, but not configured',
+            -601 => 'The website already exists on the target hosting space or server',
+            -700 => 'The email domain already exists on the target hosting space or server',
             -1100 => 'User already exists',
         ];
 
@@ -145,7 +145,6 @@ class Api
             'trace' => true,
         ]);
     }
-
 
     /**
      * Logs the last request and response if a logger is set.
