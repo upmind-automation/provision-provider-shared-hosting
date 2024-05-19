122ea76c73176572334d1ebbd8381c4d3f075131
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-05-19 19:41:57 +0300

    Update Interworx code to resolve static analysis issues and add relevant @throws

--- src/InterWorx/Api.php
+++ src/InterWorx/Api.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Upmind\ProvisionProviders\SharedHosting\InterWorx;
 
-use ErrorException;
 use Illuminate\Support\Arr;
 use Psr\Log\LoggerInterface;
 use SoapClient;
