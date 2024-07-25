59e4a7e6596818953decea595c3a1c07b38e3631
Harry Lewis <harry@upmind.com>
2024-07-25 10:16:13 +0100

    Update WHMv1/Provider::processResponse() return response_body in result data

--- src/WHMv1/Provider.php
+++ src/WHMv1/Provider.php
@@ -962,7 +962,6 @@ class Provider extends SharedHosting implements ProviderInterface
             $data = [
                 'function' => $function,
             ];
-            $debug = [];
 
             if ($e instanceof TransferException) {
                 if ($e instanceof RequestException && $e->hasResponse()) {
@@ -976,7 +975,7 @@ class Provider extends SharedHosting implements ProviderInterface
                     $data['result_data'] = $resultData;
 
                     if (!$resultData) {
-                        $debug['response_body'] = Str::limit($responseBody, 500);
+                        $data['response_body'] = Str::limit($responseBody, 500);
                     }
                 } else {
                     $message = 'WHM API Connection Error';
@@ -987,7 +986,7 @@ class Provider extends SharedHosting implements ProviderInterface
                     $message = 'WHM API Request Timeout';
                 }
 
-                $this->errorResult($message, $data, $debug, $e);
+                $this->errorResult($message, $data, [], $e);
             }
 
             throw $e;
@@ -1015,13 +1014,12 @@ class Provider extends SharedHosting implements ProviderInterface
         }
 
         $data = ['http_code' => $http_code, 'result_data' => $result_data, 'result_meta' => $result_meta];
-        $debug = [];
 
         if (empty($result_data)) {
-            $debug['response_body'] = Str::limit($response->getPsr7()->getBody()->__toString(), 300);
+            $data['response_body'] = Str::limit($response->getPsr7()->getBody()->__toString(), 300);
         }
 
-        $this->errorResult('WHM API Error: ' . $message, $data, $debug);
+        $this->errorResult('WHM API Error: ' . $message, $data);
     }
 
     protected function getSoftaculous(string $username, string $password): SoftaculousSdk
