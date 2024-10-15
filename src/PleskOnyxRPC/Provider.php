b4f0251b4c83456a69068b6c6f442b2c7ab7dc86
Harry Lewis <harry@upmind.com>
2024-10-15 17:52:40 +0100

    Update Plesk getInfo() avoid errors fetching NS when DNS service is not enabled

--- src/PleskOnyxRPC/Provider.php
+++ src/PleskOnyxRPC/Provider.php
@@ -440,7 +440,14 @@ class Provider extends SharedHosting implements ProviderInterface
                 $customerInfo = $this->getCustomerInfo($client, $customerId);
                 $username = $customerInfo->data->gen_info->getValue('login');
                 $ipAddress = $webspaceInfo->data->gen_info->getValue('dns_ip_address');
-                $domainNameServers = $this->getDnsRecords($client, $domainInfo->getValue('id'), 'NS');
+                $domainNameServers = [];
+                try {
+                    $domainNameServers = $this->getDnsRecords($client, $domainInfo->getValue('id'), 'NS');
+                } catch (Throwable $e) {
+                    if (!Str::contains($e->getMessage(), 'DNS service is not enabled')) {
+                        throw $e;
+                    }
+                }
 
                 if ($subscriptionPlan = $webspaceInfo->data->subscriptions->subscription->plan ?? null) {
                     $planInfo = $this->getPlanInfo($client, $subscriptionPlan->getValue('plan-guid'));
@@ -489,12 +496,19 @@ class Provider extends SharedHosting implements ProviderInterface
             $domainInfo = $client->customer()->request($domainRequest, Client::RESPONSE_FULL);
             $domainInfo = json_decode(json_encode($domainInfo, JSON_PRETTY_PRINT), true);
             $webspaceInfo = $client->webspace()->request($webspaceRequest);
+            $domainNameServers = [];
 
-            $domainNameServers = $this->extractNameServers(
-                (string)$webspaceInfo->data->gen_info->name,
-                $domainInfo['customer']['get-domain-list']['result']['domains'],
-                $client
-            );
+            try {
+                $domainNameServers = $this->extractNameServers(
+                    (string)$webspaceInfo->data->gen_info->name,
+                    $domainInfo['customer']['get-domain-list']['result']['domains'],
+                    $client
+                );
+            } catch (Throwable $e) {
+                if (!Str::contains($e->getMessage(), 'DNS service is not enabled')) {
+                    throw $e;
+                }
+            }
 
             $subscriptions = json_decode(json_encode($webspaceInfo->data->{'subscriptions'}, JSON_PRETTY_PRINT), true);
             $servicePlanRequest = [
@@ -558,11 +572,19 @@ class Provider extends SharedHosting implements ProviderInterface
             $webSpaceInfo = $client->webspace()->request($webspaceRequest);
             $domainInfo = $client->reseller()->request($domainRequest, Client::RESPONSE_FULL);
             $domainInfo = json_decode(json_encode($domainInfo, JSON_PRETTY_PRINT), true);
-            $domainNameServers = $this->extractNameServers(
-                (string)$webSpaceInfo->data->gen_info->name,
-                $domainInfo['reseller']['get-domain-list']['result']['domains'],
-                $client
-            );
+            $domainNameServers = [];
+
+            try {
+                $domainNameServers = $this->extractNameServers(
+                    (string)$webSpaceInfo->data->gen_info->name,
+                    $domainInfo['reseller']['get-domain-list']['result']['domains'],
+                    $client
+                );
+            } catch (Throwable $e) {
+                if (!Str::contains($e->getMessage(), 'DNS service is not enabled')) {
+                    throw $e;
+                }
+            }
 
             return AccountInfo::create(
                 [
