a00167c3dfa8e34cbe66e595e8c10f7184b1efe3
Harry Lewis <harry@upmind.com>
2024-11-13 09:40:34 +0000

    ix CustomerAddressParams to make all optional

--- src/Data/CustomerAddressParams.php
+++ src/Data/CustomerAddressParams.php
@@ -10,24 +10,24 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
 /**
  * Data representing a customer address.
  *
- * @property-read string $address_1 First line of the address
+ * @property-read string|null $address_1 First line of the address
  * @property-read string|null $address_2 Second line of the address
- * @property-read string $city City of the address
- * @property-read string $state State/region of the address
- * @property-read string $postcode Postal code of the address
- * @property-read string $country_code ISO 3166-1 alpha-2 country code
+ * @property-read string|null $city City of the address
+ * @property-read string|null $state State/region of the address
+ * @property-read string|null $postcode Postal code of the address
+ * @property-read string|null $country_code ISO 3166-1 alpha-2 country code
  */
 class CustomerAddressParams extends DataSet
 {
     public static function rules(): Rules
     {
         return new Rules([
-            'address_1' => ['required', 'string'],
+            'address_1' => ['nullable', 'string'],
             'address_2' => ['nullable', 'string'],
-            'city' => ['required', 'string'],
+            'city' => ['nullable', 'string'],
             'state' => ['nullable', 'string'],
-            'postcode' => ['required', 'string'],
-            'country_code' => ['required', 'country_code'],
+            'postcode' => ['nullable', 'string'],
+            'country_code' => ['nullable', 'country_code'],
         ]);
     }
 }
